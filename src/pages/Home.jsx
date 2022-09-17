13e4cf06c335956684a59e6665646300951fb771
ashikmahmud <ashikmamud187@gmail.com>
2022-09-18 01:21:36 +0600

    Init project named Interactly-frontend-task

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Video from "../components/Video";
+
+const Home = () => {
+  return (
+    <section className="home-page flex  justify-between sm:overflow-y-hidden">
+      <Video />
+      <div className="home-content flex-1 grid items-center justify-center">
+        <div className="btn-group flex flex-col items-stretch gap-3 ">
+          <button className="pl-3 pr-52 border rounded-full py-3 flex items-center gap-4 bg-gray-50 hover:border-fuchsia-700">
+            <span className="w-8 h-8 rounded-full bg-fuchsia-700 grid place-items-center">
+              A
+            </span>
+            <span>Campaign Structure</span>
+          </button>
+          <button className="pl-3 pr-52 border rounded-full py-3 flex items-center gap-4 bg-gray-50 hover:border-fuchsia-700">
+            <span className="w-8 h-8 rounded-full bg-fuchsia-700 grid place-items-center">
+              B
+            </span>
+            <span>Learn Facebook Basics</span>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
