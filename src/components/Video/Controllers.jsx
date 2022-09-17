0a108a1d776887228c262c490b5b48ae2d380ddb
ashikmahmud <ashikmamud187@gmail.com>
2022-09-18 02:50:44 +0600

    now try to working durations and progressbar system

--- src/components/Video/Controllers.jsx
+++ src/components/Video/Controllers.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { AiOutlineFullscreen } from "react-icons/ai";
 
-const Controllers = () => {
+const Controllers = ({
+  minutes,
+  seconds,
+  totalMinutes,
+  totalSeconds,
+  progress,
+}) => {
   return (
     <div className="video-options absolute left-0 top-0 w-full">
       {/* progressbar  */}
       <div className="progressbar -top-1 w-full h-3 bg-gray-500 relative cursor-move z-10 ">
-        <small className="progress absolute w-1/2 h-full grid place-items-center bg-violet-700 left-0 top-0"></small>
+        <small
+          className="progress absolute h-full grid place-items-center bg-violet-700 left-0 top-0"
+          style={{ width: progress + "%" }}
+        ></small>
       </div>
       {/* controls options  */}
       <div className="controls flex justify-between pt-10 px-10 z-20 relative">
@@ -18,8 +27,15 @@ const Controllers = () => {
         <div className="control-options flex items-center gap-4 cursor-pointer">
           {/* durations  */}
           <div className="durations text-white ">
-            <span className="current-time">0:00</span> /
-            <span className="total-time"> 0:00</span>
+            <span className="current-time">
+              {minutes || " 00 "}:{(seconds < 10 && "0" + seconds) || " 00 "}
+            </span>{" "}
+            /
+            <span className="total-time">
+              {" "}
+              {totalMinutes || " 00 "}:
+              {(totalSeconds < 10 && "0" + totalSeconds) || " 00 "}
+            </span>
           </div>
           {/* captions  */}
           <div className="captions">
--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,24 +1,81 @@
-import React from "react";
-import { BsPlayFill } from "react-icons/bs";
+import React, { useEffect, useRef, useState } from "react";
+import { BsFillPauseFill, BsPlayFill } from "react-icons/bs";
 import Controllers from "./Controllers";
 import Logo from "./Logo";
 const Video = () => {
+  /* operations of Video Controllers  */
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [progress, setProgress] = useState(0);
+
+  /* get video object */
+  const videoRef = useRef(null);
+
+  /* Handle Play Pause Button */
+  const handlePlayBtn = () => {
+    setInterval(() => {
+      setCurrentTime(videoRef.current.currentTime);
+    }, 1200);
+    videoRef.current.play();
+    setIsPlaying(true);
+
+    if (isPlaying) {
+      videoRef.current.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  /* Get and Handle Durations  */
+  useEffect(() => {
+    setProgress((currentTime / duration) * 100);
+    setDuration(videoRef.current.duration);
+    if ((currentTime / duration) * 100 === 100) {
+      setIsPlaying(false);
+      videoRef.current.currentTime = 0;
+      videoRef.current.pause();
+    }
+  }, [currentTime, duration]);
+
+  console.log(progress);
+
+  /* All Organized Time */
+  const minutes = Math.floor(currentTime / 60);
+  const seconds = Math.floor(currentTime % 60);
+  const totalMinutes = Math.floor(duration / 60);
+  const totalSeconds = Math.floor(duration % 60);
+
   return (
     <div className="h-screen  border relative flex-1 grid place-items-center">
-      <Controllers />
+      <Controllers
+        minutes={minutes}
+        seconds={seconds}
+        totalMinutes={totalMinutes}
+        totalSeconds={totalSeconds}
+        progress={progress}
+      />
       {/* play btn  */}
 
       <video
-        autoPlay={true}
         className="w-screen h-screen object-cover cursor-pointer"
+        ref={videoRef}
+        onClick={handlePlayBtn}
+        onEnded={() => setIsPlaying(false)}
       >
         <source src="https://media.videoask.com/transcoded/dabd0292-cf99-40ba-a12a-245a279b31dc/video.mp4?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJtZWRpYV9pZCI6ImRhYmQwMjkyLWNmOTktNDBiYS1hMTJhLTI0NWEyNzliMzFkYyIsImV4cCI6MTY2Mzc3MjE1NH0.sut40p-XhaMDOJL22Fh9dc1f1aj8vOm5oH_ZO0CHs4COrWIDr61tFtfdhaEHJOfZtrWvVao019c4aTqII7CSANLhpcNRE4DKTGlTDP1Mv_riR_GpPwRhqTVnYYrQIX-2gwgOIgmVggpnZJE_gUTddjSlX77-4iqb_k1oPbLy8Hd4fY5ZC3UCLzQz-86OUyhAmaWQwdt0-0V0ynMj_OuVDz4e4pL5xUJobI4ZenGCdm5iNk87JcefFYzcjJLLKO2FvWOoHsq9txrM5NsRZPg-ts1K_fsCnHY1fLrc1pUcjtnfEatklf9SKvoCfw0LbvWCpTq6dVRJfywh1FDmZt2UYbVoebmcmN6Y_2FjWKuMNK2oEVtk8IsXE8rxP718IaZI0MsH-hKT84bvbowWFgeyHqQByuXWIQskPKus7zkVKIDkokkcSyLQTeMEP0FCoGkxhMG4vBGgZIlBppyUe9x4pjo1Ost8EhvZRLTA2Qy-sjjznaib5mRAmgcD64tLvX86uTMnokkpLn5xDNif5XMOyNzRVQk_3OUNZMHUjR4C61UyB7SYlJ2GfP7HYPO2aFwKeWUiSaOHyhzPf1riFTRPpxhE4OEcguLrQt9YmwOSdxV-rVLiOPggWrZDvHxZbNIvCLw9fmjDCOxypu1l7aEkLT4-WNkEzI29IUuI8cwi1Ss" />
       </video>
-      <div className="play-btn absolute z-50 cursor-pointer">
-        <button className="play-btn-icon bg-white rounded-full w-16 h-16 grid place-items-center text-4xl">
-          <BsPlayFill />
-        </button>
-      </div>
+
+      {!isPlaying && (
+        <div className="play-btn absolute z-50 cursor-pointer">
+          <button
+            className="play-btn-icon bg-white rounded-full w-16 h-16 grid place-items-center text-4xl"
+            onClick={handlePlayBtn}
+          >
+            {isPlaying ? <BsFillPauseFill /> : <BsPlayFill />}
+          </button>
+        </div>
+      )}
+
       {/* logo  */}
       <Logo />
     </div>
