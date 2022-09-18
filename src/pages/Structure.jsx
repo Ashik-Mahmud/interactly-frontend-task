d1c71926fad36fba0c757747cc4b62e993239a64
ashikmahmud <ashikmamud187@gmail.com>
2022-09-18 06:32:01 +0600

    done responsiveness

--- src/pages/Structure.jsx
+++ src/pages/Structure.jsx
@@ -4,10 +4,10 @@ import Video from "../components/Video/Video";
 
 const Structure = ({ src, btnText, head }) => {
   return (
-    <section className="home-page flex  justify-between sm:overflow-y-hidden">
+    <section className="home-page sm:flex justify-between overflow-y-hidden">
       <Video heading={head} src={src} />
 
-      <div className="home-content flex-1 grid items-center justify-center">
+      <div className="home-content  absolute w-full bottom-20 pl-3 sm:relative flex-1 grid items-center justify-center">
         <div className="btn-group flex flex-col items-stretch gap-3 ">
           <SecondaryButton className="btn">{btnText}</SecondaryButton>
         </div>
