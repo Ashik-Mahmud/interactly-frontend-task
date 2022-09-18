beaaff9d64ab5e2e509c9f46b4934851515e501b
ashikmahmud <ashikmamud187@gmail.com>
2022-09-18 19:15:40 +0600

    add animation for looking great

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,57 +1,62 @@
 import React from "react";
 import { BsArrowDownRight } from "react-icons/bs";
-
+import Fade from "react-reveal/Fade";
 const Form = () => {
   return (
-    <div className="p-10 bg-white z-50 sm:px-24 w-full h-screen sm:h-auto fixed  left-0 top-0 sm:relative">
-      <form action="">
-        <h3 className="text-2xl font-medium">
-          Before you go, please leave your contact details so we can get back to
-          you...
-        </h3>
-        <div className="input-group w-full flex flex-col gap-6 my-5">
-          <input
-            type="text"
-            className="w-full text-xl border-t-none border-b border-b-gray-400 p-3 outline-none "
-            placeholder="*Your Name"
-          />
-          <input
-            type="email"
-            className="w-full text-xl border-t-none border-b border-b-gray-400 p-3 outline-none "
-            placeholder="*Your Email"
-          />
-        </div>
+    <Fade top distance="40px">
+      <div className="p-10 bg-white z-50 sm:px-24 w-full h-screen sm:h-auto fixed  left-0 top-0 sm:relative">
+        <form action="">
+          <h3 className="text-2xl font-medium">
+            Before you go, please leave your contact details so we can get back
+            to you...
+          </h3>
+          <div className="input-group w-full flex flex-col gap-6 my-5">
+            <input
+              type="text"
+              className="w-full text-xl border-t-none border-b border-b-gray-400 p-3 outline-none "
+              placeholder="*Your Name"
+              required
+            />
+            <input
+              type="email"
+              className="w-full text-xl border-t-none border-b border-b-gray-400 p-3 outline-none "
+              placeholder="*Your Email"
+              required
+            />
+          </div>
 
-        <div className="input-group flex items-start mt-9 gap-3">
-          <input
-            type="checkbox"
-            name=""
-            style={{ width: "20px", height: "20px" }}
-            id="condition"
-          />
-          <label
-            htmlFor="condition"
-            className="text-sm h-28 overflow-auto sm:h-auto sm:overflow-hidden sm:text-base leading-relaxed text-gray-600"
-          >
-            * [Sample legal text] The personal data you have provided will be
-            processed by XXXXX for purposes of providing you the service. The
-            legal basis of the processing is XXXXX. Your data will not be
-            transferred nor assigned to third parties. You can exercise your
-            right to access, rectify and delete your data, as well as the other
-            rights granted by law by sending an email to XXXXX. For further
-            information, please check our privacy policy XXXXX.
-          </label>
-        </div>
-        <div className="btn relative z-20">
-          <button
-            type="button"
-            className="text-white bg-[#7d00fe] py-5 w-full rounded-md flex items-center gap-3 justify-center mt-7 text-xl hover:scale-100 duration-75 hover:bg-[##912ef5]"
-          >
-            Send Your Answer <BsArrowDownRight />
-          </button>
-        </div>
-      </form>
-    </div>
+          <div className="input-group flex items-start mt-9 gap-3">
+            <input
+              type="checkbox"
+              name=""
+              style={{ width: "20px", height: "20px" }}
+              id="condition"
+              required
+            />
+            <label
+              htmlFor="condition"
+              className="text-sm h-28 overflow-auto sm:h-auto sm:overflow-hidden sm:text-base leading-relaxed text-gray-600"
+            >
+              * [Sample legal text] The personal data you have provided will be
+              processed by XXXXX for purposes of providing you the service. The
+              legal basis of the processing is XXXXX. Your data will not be
+              transferred nor assigned to third parties. You can exercise your
+              right to access, rectify and delete your data, as well as the
+              other rights granted by law by sending an email to XXXXX. For
+              further information, please check our privacy policy XXXXX.
+            </label>
+          </div>
+          <div className="btn relative z-20">
+            <button
+              type="submit"
+              className="text-white bg-[#7d00fe] py-5 w-full rounded-md flex items-center gap-3 justify-center mt-7 text-xl hover:scale-100 duration-75 hover:bg-[##912ef5]"
+            >
+              Send Your Answer <BsArrowDownRight />
+            </button>
+          </div>
+        </form>
+      </div>
+    </Fade>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,28 @@
 import React from "react";
+import { Fade } from "react-reveal";
 import Button from "../components/Button";
 import Video from "../components/Video/Video";
 
 const Home = () => {
   return (
     <>
-      <section className="home-page sm:flex justify-between overflow-y-hidden">
-        <Video
-          heading={"Welcome"}
-          src={
-            "https://media.videoask.com/transcoded/dabd0292-cf99-40ba-a12a-245a279b31dc/video.mp4?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJtZWRpYV9pZCI6ImRhYmQwMjkyLWNmOTktNDBiYS1hMTJhLTI0NWEyNzliMzFkYyIsImV4cCI6MTY2Mzc3MjE1NH0.sut40p-XhaMDOJL22Fh9dc1f1aj8vOm5oH_ZO0CHs4COrWIDr61tFtfdhaEHJOfZtrWvVao019c4aTqII7CSANLhpcNRE4DKTGlTDP1Mv_riR_GpPwRhqTVnYYrQIX-2gwgOIgmVggpnZJE_gUTddjSlX77-4iqb_k1oPbLy8Hd4fY5ZC3UCLzQz-86OUyhAmaWQwdt0-0V0ynMj_OuVDz4e4pL5xUJobI4ZenGCdm5iNk87JcefFYzcjJLLKO2FvWOoHsq9txrM5NsRZPg-ts1K_fsCnHY1fLrc1pUcjtnfEatklf9SKvoCfw0LbvWCpTq6dVRJfywh1FDmZt2UYbVoebmcmN6Y_2FjWKuMNK2oEVtk8IsXE8rxP718IaZI0MsH-hKT84bvbowWFgeyHqQByuXWIQskPKus7zkVKIDkokkcSyLQTeMEP0FCoGkxhMG4vBGgZIlBppyUe9x4pjo1Ost8EhvZRLTA2Qy-sjjznaib5mRAmgcD64tLvX86uTMnokkpLn5xDNif5XMOyNzRVQk_3OUNZMHUjR4C61UyB7SYlJ2GfP7HYPO2aFwKeWUiSaOHyhzPf1riFTRPpxhE4OEcguLrQt9YmwOSdxV-rVLiOPggWrZDvHxZbNIvCLw9fmjDCOxypu1l7aEkLT4-WNkEzI29IUuI8cwi1Ss"
-          }
-        />
+      <Fade top distance="20px">
+        <section className="home-page sm:flex justify-between overflow-y-hidden">
+          <Video
+            heading={"Welcome"}
+            src={
+              "https://media.videoask.com/transcoded/dabd0292-cf99-40ba-a12a-245a279b31dc/video.mp4?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJtZWRpYV9pZCI6ImRhYmQwMjkyLWNmOTktNDBiYS1hMTJhLTI0NWEyNzliMzFkYyIsImV4cCI6MTY2Mzc3MjE1NH0.sut40p-XhaMDOJL22Fh9dc1f1aj8vOm5oH_ZO0CHs4COrWIDr61tFtfdhaEHJOfZtrWvVao019c4aTqII7CSANLhpcNRE4DKTGlTDP1Mv_riR_GpPwRhqTVnYYrQIX-2gwgOIgmVggpnZJE_gUTddjSlX77-4iqb_k1oPbLy8Hd4fY5ZC3UCLzQz-86OUyhAmaWQwdt0-0V0ynMj_OuVDz4e4pL5xUJobI4ZenGCdm5iNk87JcefFYzcjJLLKO2FvWOoHsq9txrM5NsRZPg-ts1K_fsCnHY1fLrc1pUcjtnfEatklf9SKvoCfw0LbvWCpTq6dVRJfywh1FDmZt2UYbVoebmcmN6Y_2FjWKuMNK2oEVtk8IsXE8rxP718IaZI0MsH-hKT84bvbowWFgeyHqQByuXWIQskPKus7zkVKIDkokkcSyLQTeMEP0FCoGkxhMG4vBGgZIlBppyUe9x4pjo1Ost8EhvZRLTA2Qy-sjjznaib5mRAmgcD64tLvX86uTMnokkpLn5xDNif5XMOyNzRVQk_3OUNZMHUjR4C61UyB7SYlJ2GfP7HYPO2aFwKeWUiSaOHyhzPf1riFTRPpxhE4OEcguLrQt9YmwOSdxV-rVLiOPggWrZDvHxZbNIvCLw9fmjDCOxypu1l7aEkLT4-WNkEzI29IUuI8cwi1Ss"
+            }
+          />
 
-        <div className="home-content absolute w-full bottom-20 pl-3 sm:relative flex-1 grid items-center justify-center">
-          <div className="btn-group flex flex-col items-stretch gap-3 ">
-            <Button label={"A"} text="Campaign Structure" />
-            <Button label={"B"} text="Learn Facebook Basics" />
+          <div className="home-content absolute w-full bottom-20 pl-3 sm:relative flex-1 grid items-center justify-center">
+            <div className="btn-group flex flex-col items-stretch gap-3 ">
+              <Button label={"A"} text="Campaign Structure" />
+              <Button label={"B"} text="Learn Facebook Basics" />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </Fade>
     </>
   );
 };
